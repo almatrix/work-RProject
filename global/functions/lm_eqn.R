8dc893d13811d54e8d93d3d18152214ac0223687
almatrix <mingli.sgg@gmail.com>
2015-02-17 17:27:14 +0100

    update

---
+++ global/functions/lm_eqn.R
@@ -0,0 +1,29 @@
+lm_eqn = function(df,x,y){
+    m = lm(y ~ x, df);
+#     eq <- substitute(atop(italic(y) == a + b ~ italic(x),
+#                           italic(r)^2~"="~r2*","~~italic(p)~"="~pvalue), 
+
+#     eq <- substitute(atop(italic(y) == a + b ~ italic(x)*","~~italic(r)^2~"="~r2*","~~italic(p)~"="~pvalue), 
+    eq <- substitute(italic(y) == a + b ~ italic(x)*","~~italic(r)^2~"="~r2,
+                     list(a = format(coef(m)[1], digits = 2), 
+                          b = format(coef(m)[2], digits = 2), 
+                          r2 = format(summary(m)$r.squared, digits = 3),
+                          pvalue = format(summary(m)[[4]][[8]],scientific=TRUE, digits = 3)))
+    as.character(as.expression(eq));                 
+}
+
+lm_eqn_log= function(df,x,y){
+    m = lm(y ~ log(x), df);
+    #     eq <- substitute(atop(italic(y) == a + b ~ italic(x),
+    #                           italic(r)^2~"="~r2*","~~italic(p)~"="~pvalue), 
+    
+    #     eq <- substitute(atop(italic(y) == a + b ~ italic(x)*","~~italic(r)^2~"="~r2*","~~italic(p)~"="~pvalue), 
+    eq <- substitute(italic(y) == a + b %.% log ( italic(x) )*","~~italic(r)^2~"="~r2,
+                     list(a = format(coef(m)[1], digits = 2), 
+                          b = format(coef(m)[2], digits = 2), 
+                          r2 = format(summary(m)$r.squared, digits = 3),
+                          pvalue = format(summary(m)[[4]][[8]],scientific=TRUE, digits = 3)))
+    as.character(as.expression(eq));                 
+}
+
+
