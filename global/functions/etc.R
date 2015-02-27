bac8c2a5dd154c8f880e691e7dd3a0c86f9a251f
almatrix <mingli.sgg@gmail.com>
2015-02-27 11:20:50 +0100

    IJGIS

--- global/functions/etc.R
+++ global/functions/etc.R
@@ -1,3 +1,15 @@
 format.percent <- function(x, digits = 2, format = "f", ...) {
     paste0(formatC(100 * x, format = format, digits = digits, ...), "%")
 }
+
+counter.print = function(interval){
+    if(counter %% interval ==0) print(paste(Sys.time(),counter))
+    counter <<- counter+1
+}
+
+counter.reset = function(){counter <<- 1}
+
+time.print = function(ts_now, ts_last, str_desc){
+    print(paste(ts_now, str_desc, 
+                as.integer(ts_now)-as.integer(ts_last), "seconds.") )
+}
